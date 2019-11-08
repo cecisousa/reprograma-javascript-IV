2e6a6c928306e28cc64869830525fe6591966e46
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-11-08 11:45:23 -0300

    adiciona novo exemplo de post api

--- fetch/ex03Post/main.js
+++ fetch/ex03Post/main.js
@@ -2,14 +2,33 @@ const titulo = document.getElementById("titulo");
 const descricao = document.getElementById("descricao");
 const btnSubmit = document.getElementById("btn-submit");
 
+// btnSubmit.addEventListener("click", function(e){
+//     e.preventDefault();
+//     let body = JSON.stringify({
+//         title: titulo.value,
+//         body: descricao.value,
+//         userId: 1
+//     })
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/json"
+//         }, 
+//         body 
+//     }).then(response => response.json()).then(json => console.log(json))
+// });
+
+// Segunda versão, enviando um comentário:
+
 btnSubmit.addEventListener("click", function(e){
     e.preventDefault();
     let body = JSON.stringify({
-        title: titulo.value,
-        body: descricao.value,
+        name: "Ceci Sousa",
+        email: "ceci@gmail.com",
+        body: "Estou enviando um comentário",
         userId: 1
     })
-    fetch("https://jsonplaceholder.typicode.com/posts", {
+    fetch("https://jsonplaceholder.typicode.com/comments", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
@@ -18,3 +37,4 @@ btnSubmit.addEventListener("click", function(e){
     }).then(response => response.json()).then(json => console.log(json))
 });
 
+// OBS.: É preciso ver na API quais itens são obrigatórios na construção do 'body'
