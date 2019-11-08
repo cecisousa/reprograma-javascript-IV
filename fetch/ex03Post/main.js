c1a676ef18dc0415dc411e9d526897a23ad5c130
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-11-08 11:22:12 -0300

    realiza exercicio de post na api

---
+++ fetch/ex03Post/main.js
@@ -0,0 +1,20 @@
+const titulo = document.getElementById("titulo");
+const descricao = document.getElementById("descricao");
+const btnSubmit = document.getElementById("btn-submit");
+
+btnSubmit.addEventListener("click", function(e){
+    e.preventDefault();
+    let body = JSON.stringify({
+        title: titulo.value,
+        body: descricao.value,
+        userId: 1
+    })
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        }, 
+        body 
+    }).then(response => response.json()).then(json => console.log(json))
+});
+
